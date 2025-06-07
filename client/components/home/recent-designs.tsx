a5943ff644fd375c7d9a3094b20a5824e7b41d90
Bishal Moktan <bishalmoktan270@gmail.com>
2025-06-07 21:55:37 +0545

    Complete home UI

---
+++ client/components/home/recent-designs.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+function RecentDesigns() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Recent Designs</h2>
+    </div>
+  );
+}
+
+export default RecentDesigns;
